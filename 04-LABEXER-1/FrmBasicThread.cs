987a54dfe24524aafde9459d07ba0751f8e5faeb
jassyv4 <kjasvillanovar@gmail.com>
2025-09-18 18:12:59 +0800

    Add project files.

---
+++ 04-LABEXER-1/FrmBasicThread.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04_LABEXER_1
+{
+    public partial class FrmBasicThread : Form
+    {
+        public FrmBasicThread()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 04-LABEXER-1/MyThreadClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_LABEXER_1
+{
+    internal class MyThreadClass
+    {
+        public static Thread1;
+    }
+}
